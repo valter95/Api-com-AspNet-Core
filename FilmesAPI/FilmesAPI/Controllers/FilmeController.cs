f6a8ae35c101d2feb2e7e42b705469da742345dd
Valter <valterhenrique39@gmail.com>
2022-03-08 16:56:17 -0300

    Incio do versionamento do projeto.

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,38 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route ("[controller]")] // Chama a classe FilmeController 
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme) 
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes() 
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmesPorId(int id) 
+        {
+           return filmes.FirstOrDefault(filme => filme.Id == id);
+        }
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required (ErrorMessage = "O campo título é obrigatorio")] //Criando uma validação para não vir o titulo vazio 
+        public string Titulo { get; set; }
+        [Required (ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+        public int Id { get; internal set; }
+    }
+}
